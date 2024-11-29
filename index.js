67087c0b86035611ed0bab70d3eb5f6cf06882f5
Martha Wangui Mwangi <marthawm7252@gmail.com>
2024-11-29 19:36:09 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+         customerName= customerName.toLocaleUpperCase();
+         return customerName;
+     }
+
+
+var bestCustomer;
+function setBestCustomer(){
+    // bestCustomer = "not bob"
+    return bestCustomer="not bob";
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer ="maybe bob"
+    return bestCustomer;
+}
+
+
+const leastFavoriteCustomer ='Jane';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'John'
+
+}
